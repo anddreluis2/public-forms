ef46c7ccac51fb1d595e84a8d505569a9eb603d3
André Luis de Oliveira <65047921+anddreluis2@users.noreply.github.com>
2025-05-28 21:08:00 -0300

    Update getInstruments function to use environment variable for base URL
    - Changed the base URL in the getInstruments function to utilize process.env.BASE_URL for improved configurability.
    - Added a console log to output the current base URL for debugging purposes.

--- app/lib/instruments.ts
+++ app/lib/instruments.ts
@@ -1,7 +1,8 @@
 import { Instrument } from "@/app/types";
 
 export async function getInstruments(): Promise<Instrument[]> {
-  const baseUrl = "http://ht-prod-backend.fly.dev";
+  const baseUrl = process.env.BASE_URL;
+  console.log(process.env.BASE_URL);
   const response = await fetch(`${baseUrl}/forms/public/library/`, {
     cache: "no-store",
   });
