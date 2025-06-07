c33bcbd6cc0ec566464e62964e141d4cc593bfbf
André Luis de Oliveira <65047921+anddreluis2@users.noreply.github.com>
2025-06-07 12:58:54 -0300

    Update getInstruments function to change API endpoint and implement caching
    - Modified the fetch call in getInstruments to use the new endpoint for retrieving instrument data.
    - Added caching with a revalidation period of 3600 seconds to improve performance.

--- app/lib/instruments.ts
+++ app/lib/instruments.ts
@@ -3,8 +3,10 @@ import { Instrument } from "@/app/types";
 const baseUrl = process.env.BASE_URL;
 
 export async function getInstruments(): Promise<Instrument[]> {
-  const response = await fetch(`${baseUrl}/forms/public/library/`, {
-    cache: "no-store",
+  const response = await fetch(`${baseUrl}/public/forms/library/`, {
+    next: {
+      revalidate: 3600,
+    },
   });
 
   if (!response.ok) {
